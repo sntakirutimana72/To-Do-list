665ebdc35b8cdc0feaf88efa5cb6e9649720b661
Steven Ntakirutimana <51259659+sntakirutimana72@users.noreply.github.com>
2022-06-02 18:57:43 +0200

    Revert "Revert "Milestone 3: Interactive List""

--- src/index.js
+++ src/index.js
@@ -1,103 +1,25 @@
+import {
+  create, populate, remove, enableEdit, setDescription,
+} from './modules/CRUD.js';
+import setState from './modules/set-state.js';
+import propClearTaskTrigger from './modules/actions.js';
+import $select from './modules/selectors.js';
 import './index.css';
 
-const toDoList = [
-  {
-    index: '01',
-    description: 'Do physical exercises',
-    completed: true,
-  },
-  {
-    index: '02',
-    description: 'Do the dishes',
-    completed: false,
-  },
-  {
-    index: '03',
-    description: 'Make breakfast',
-    completed: false,
-  },
-  {
-    index: '04',
-    description: 'Prepare lunch',
-    completed: false,
-  },
-  {
-    index: '05',
-    description: 'Go to the bank',
-    completed: true,
-  },
-];
-
-let indexCount = 6;
-
-const toDoListContainer = document.body.querySelector('.to-do-list');
-
-function toggleTaskState({ parentElement: task }) {
-  task.classList.toggle('status-completed');
-}
-
-function propClearBtn() {
-  const clearButton = document.body.querySelector('.to-do-clear-btn');
-
-  if (clearButton.hasAttribute('disabled')) {
-    clearButton.removeAttribute('disabled');
-  } else clearButton.setAttribute('disabled', 'true');
-}
-
-function render({ index, description, completed: state }) {
-  const component = document.createElement('li');
-  component.id = index;
-  component.className = 'row';
-
-  if (state) component.classList.add('status-completed');
-
-  component.innerHTML = `
-    <input type='checkbox'${state ? ' checked' : ''}>
-    <label>${description}</label>
-    <button type='button'><i class='fa-solid fa-ellipsis-vertical'></i></button>
-  `;
-
-  return component;
-}
-
-function createTask(event) {
-  event.preventDefault();
-
-  toDoListContainer.appendChild(render({
-    index: indexCount,
-    description: this.elements.desc.value,
-    completed: false,
-  }));
-
-  indexCount += 1;
-
-  this.reset();
-}
-
-function populateToDoList() {
-  return new Promise((resolve, rej) => {
-    toDoList.forEach((task) => {
-      toDoListContainer.appendChild(render(task));
-    });
-
-    resolve();
-  });
-}
-
 window.addEventListener('DOMContentLoaded', () => {
-  populateToDoList().then(propClearBtn);
+  populate().then(propClearTaskTrigger);
 
-  document.forms[0].addEventListener('submit', createTask);
+  document.forms[0].addEventListener('submit', create);
 
-  document.body.querySelector('.to-do-clear-btn').onclick = () => {
-    Array.from(toDoListContainer.children).forEach((task) => {
-      if (task.children[0].checked) toDoListContainer.removeChild(task);
-    });
-  };
+  $select('.to-do-clear-btn').onclick = () => remove();
 
   document.body.addEventListener('click', ({ target }) => {
-    if (target.tagName === 'INPUT' && target.type === 'checkbox') {
-      toggleTaskState(target);
-    }
+    if (target.classList.contains('rm-btn')) enableEdit(target);
+  });
+
+  document.body.addEventListener('change', ({ target }) => {
+    if (target.classList.contains('field-desc')) setDescription(target);
+
+    else if (target.classList.contains('toggler')) setState(target);
   });
 });
---
+++ src/modules/selectors.js
@@ -0,0 +1,11 @@
+const $select = (selector, all, parentTree) => {
+  parentTree = document.body;
+
+  if (!parentTree) parentTree = document.body;
+
+  if (all === true) return parentTree.querySelectorAll(selector);
+
+  return parentTree.querySelector(selector);
+};
+
+export default $select;
